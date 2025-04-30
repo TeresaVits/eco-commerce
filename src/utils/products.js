ad98978f047e55e7d11dce955ff84ee7d7222a5f
TeresaVits <teresarlacerda@gmail.com>
2025-04-30 18:30:41 -0300

    first commit

---
+++ src/utils/products.js
@@ -0,0 +1,132 @@
+
+
+import banner1 from "../Images/banner1.jpg";
+import banner2 from "../Images/banner2.jpg";
+import banner3 from "../Images/banner3.jpg";
+export const SliderData = [
+  {
+      id: 1,
+      title: "Compre com Propósito",
+      desc: "Escolha produtos sustentáveis e saiba exatamente o impacto positivo que sua compra gera no meio ambiente.",
+      cover: banner1,
+    },
+    {
+      id: 2,
+      title: "Sustentabilidade na Sua Rotina",
+      desc: "Encontre itens eco-friendly de forma simples e rápida. Comprar de forma consciente nunca foi tão fácil.",
+      cover: banner2,
+    },
+    {
+      id: 3,
+      title: "Transparência em Cada Pedido",
+      desc: "Acompanhe a entrega do seu pedido e veja o quanto sua escolha contribui para um planeta melhor.",
+      cover: banner3,
+    }
+      
+];
+
+
+export const discoutProducts = [
+  {
+    id: "01",
+    productName: "Product 1",
+    category: "sofa", 
+    price: 193,
+    discount:30,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+   
+  },
+  {
+    id: "02",
+    productName: "Product 2 ",
+    category: "sofa",
+    price: 253,
+    discount:20,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+    
+  },
+  {
+    id: "08",
+    productName: "Product 3",
+
+    category: "chair",
+    price: 89,
+    discount:15,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+
+  },
+  {
+    id: "09",
+    productName: "Product 4",
+    category: "chair",
+    price: 112,
+    discount:35,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+   
+  },
+  {
+    id: "12",
+    productName: "Product 5",
+    category: "mobile",
+    price: 599,
+    discount:10,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+    
+  },
+  {
+    id: "13",
+    productName: "Product 6",
+    category: "mobile",
+    price: 799,
+    discount:5,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+   
+  },
+
+]
+
+export const products = [
+  {
+    id: "01",
+    productName: "Product 1",
+    category: "sofa",
+    price: 193,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+  
+  },
+
+  {
+    id: "02",
+    productName: "Product 2",
+    category: "sofa",
+    price: 253,
+    shortDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+   
+  },
+
+  
+];
