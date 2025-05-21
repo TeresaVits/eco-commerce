9618d0d198fcfc15be8136f9489456d68f0aa8a8
TeresaVits <teresarlacerda@gmail.com>
2025-05-21 00:11:01 -0300

    cria endpoint para consumir produtos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,38 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 
 import Section from "../components/Section";
 import { products, discoutProducts } from "../utils/products";
+
 import SliderHome from "../components/Slider";
 import useWindowScrollToTop from "../hooks/useWindowScrollToTop";
-
+import { listarProdutos } from "../service/produtoService";
 const Home = () => {
-  const newArrivalData = products.filter(
-    (item) => item.category === "mobile" || item.category === "wireless"
-  );
-  const bestSales = products.filter((item) => item.category === "sofa");
+  const newArrivalData = [];
+  const bestSales = [];
+
   useWindowScrollToTop();
+
+  useEffect(() => {
+    listarProdutos().then((produtos) => {
+      console.log("📦 Produtos retornados do backend:");
+      console.table(produtos); // Mostra em formato de tabela no console
+    }).catch((err) => {
+      console.error("Erro ao buscar produtos do backend:", err);
+    });
+  }, []);
+
   return (
     <Fragment>
       <SliderHome />
-   
+
       <Section
         title="Itens"
         bgColor="#f6f9fc"
-        productItems={discoutProducts}
+        productItems={[]} // Aqui você pode usar produtos do backend se quiser
+        //abaixo é o código pra ver os produtos estáticos
+        // productItems={discoutProducts}
+
       />
-     
     </Fragment>
   );
 };
